7ea3b6c2bb0d4256d9151d903865c48c641722dc
Emir <emirmarash@gmail.com>
2026-02-17 12:13:30 +0600

    Enhance i18n with profile nav and hero descriptions
    Added a profile navigation item and descriptions in hero sections for multiple languages.

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -4,22 +4,22 @@ import { initReactI18next } from "react-i18next";
 const resources = {
   ru: {
     translation: {
-      nav: { home: "Главная", map: "Карта", rank: "Топ", fund: "Фонд" },
-      hero: { title: "Волна перемен", subtitle: "в городе Ош" },
+      nav: { home: "Главная", map: "Карта", rank: "Топ", fund: "Фонд", profile: "Профиль" },
+      hero: { title: "Волна перемен", subtitle: "в городе Ош",description:"Мы создаем новую культуру городского пространства. Присоединяйся к волонтерам, которые делают свой дом чище каждый день." },
       footer: { dev: "DEVELOPED BY", bio: "Frontend Developer & Eco-activist" }
     }
   },
   en: {
     translation: {
       nav: { home: "Home", map: "Map", rank: "Top", fund: "Fund" },
-      hero: { title: "Wave of Change", subtitle: "in Osh city" },
+      hero: { title: "Wave of Change", subtitle: "in Osh city", description:"We're creating a new culture of urban space. Join the volunteers who make their homes cleaner every day."  },
       footer: { dev: "DEVELOPED BY", bio: "Frontend Developer & Eco-activist" }
     }
   },
   kg: {
     translation: {
       nav: { home: "Башкы", map: "Карта", rank: "Мыктылар", fund: "Фонд" },
-      hero: { title: "Өзгөрүү толкуну", subtitle: "Ош шаарында" },
+      hero: { title: "Өзгөрүү толкуну", subtitle: "Ош шаарында", description:"Биз шаардык мейкиндиктин жаңы маданиятын түзүп жатабыз. Үйлөрүн күн сайын таза кылган ыктыярчыларга кошулуңуз." },
       footer: { dev: "ИШТЕП ЧЫККАН", bio: "Frontend Developer & Эко-активист" }
     }
   }
