935a60860aa8d8a8748eb80d948810f8c08c1411
Emir <emirmarash@gmail.com>
2026-02-17 12:31:30 +0600

    Update i18n.js

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -11,14 +11,14 @@ const resources = {
   },
   en: {
     translation: {
-      nav: { home: "Home", map: "Map", rank: "Top", fund: "Fund" },
+      nav: { home: "Home", map: "Map", rank: "Top", fund: "Fund", profile:"Профиль" },
       hero: { title: "Wave of Change", subtitle: "in Osh city", description:"We're creating a new culture of urban space. Join the volunteers who make their homes cleaner every day."  },
       footer: { dev: "DEVELOPED BY", bio: "Frontend Developer & Eco-activist" }
     }
   },
   kg: {
     translation: {
-      nav: { home: "Башкы", map: "Карта", rank: "Мыктылар", fund: "Фонд" },
+      nav: { home: "Башкы", map: "Карта", rank: "Мыктылар", fund: "Фонд", profile:"Profile" },
       hero: { title: "Өзгөрүү толкуну", subtitle: "Ош шаарында", description:"Биз шаардык мейкиндиктин жаңы маданиятын түзүп жатабыз. Үйлөрүн күн сайын таза кылган ыктыярчыларга кошулуңуз." },
       footer: { dev: "ИШТЕП ЧЫККАН", bio: "Frontend Developer & Эко-активист" }
     }
@@ -34,3 +34,4 @@ i18n
   });
 
 export default i18n;
+
